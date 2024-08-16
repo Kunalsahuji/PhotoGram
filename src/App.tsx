1818c828156729f818a9ef1888ad0d8a1fbbafe4
Kunalsahuji <ksahu0103@gmail.com>
2024-08-16 15:40:46 +0530

    Authentication | Loading...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { UserAuthProvider } from './context/userAuthContext';
 
 interface IAppProps {
 }
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
   return (
-    <RouterProvider router={router} /> 
-  );
+    <UserAuthProvider>
+      <RouterProvider router={router} />
+    </UserAuthProvider>
+  ); 
 };
 
 export default App;
--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth } from 'firebase/auth';
+
 interface IProtectedRoutesProps {
 }
 
 const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
-    const isAuth: boolean = false
-    const location = useLocation()
-    return isAuth ? (<Outlet />) : (
+    const auth = getAuth();
+    const [user, loading] = useAuthState(auth)
+    const location = useLocation();
+
+    if (loading) {
+        return <div>...loading</div>
+    }
+    return user ? (<Outlet />) : (
         <Navigate to="/login" state={{ from: location }
-        } /> 
+        } />
     );
 };
 
