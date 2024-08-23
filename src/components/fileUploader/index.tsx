73547710fcaf7f94e495876d08efd9b5d48e9415
Kunalsahuji <ksahu0103@gmail.com>
2024-08-23 16:12:37 +0530

    post

---
+++ src/components/fileUploader/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IFileUploaderProps {
+}
+
+const FileUploader: React.FunctionComponent<IFileUploaderProps> = () => {
+  return ;
+};
+
+export default FileUploader;
+
+
