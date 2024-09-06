647e6fb5f4b178aecf59e8da0c263cb75a1d6d11
Kunalsahuji <ksahu0103@gmail.com>
2024-09-06 17:05:49 +0530

    Post | textarea | Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { OutputFileEntry } from "@uploadcare/react-uploader";
+
 export interface UserSignIn {
     email: string;
     password: string;
@@ -7,3 +9,29 @@ export interface UserLogIn {
     email: string;
     password: string;
 }
+export interface FileEntry {
+    files: OutputFileEntry[];
+}
+export interface Post {
+    caption: string;
+    photos: PhotoMeta[];
+    likes: number;
+    userlikes: [];
+    userId: string | null;
+    date: Date;
+}
+export interface PhotoMeta{
+    cdnUrl : string;
+    uuid: string;
+}
+
+export interface DocumentResponse {
+    id: string;
+    caption: string;
+    photos: PhotoMeta[];
+    likes: number;
+    userlikes: [];
+    userId: string | null;
+    date: Date;
+  }
+  
